d8347b8a95af5d715880359e77d20606b6e82978
Clauber Martins Pinto <clauber.pinto@mapa.magov.intranet>
2022-10-14 15:06:58 -0300

    Aula34 Filas: Introdução

---
+++ src/com/clauber/estruturadados/fila/Fila.java
@@ -0,0 +1,14 @@
+package com.clauber.estruturadados.fila;
+
+import com.clauber.estruturadados.base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+
+	public Fila() {
+		super();
+	}
+	
+	public Fila(int capacidade) {
+		super(capacidade);
+	}
+}
